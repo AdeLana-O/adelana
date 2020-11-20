7b1049a069dfa1e878bc022ed8ade5c39eb59e25
ade_lana <lanadvictor@yahoo.co.uk>
2020-11-20 13:28:12 +0100

    updates

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,18 +1,24 @@
 import React from "react";
+
 import ContactMenu from "../ContactMenu";
 
 const Hero = () => {
+
   return (
     <section id="hero">
       <div className="container">
         <div className="hero__container">
-          Hi! I'm <h1>Adelana Onafuwa</h1>.
-          <p>Software Developer and lover of JavaScript.</p>
-          <ContactMenu />
+          <div className="hero__info">
+            <h1>adelana onafuwa</h1>
+            <p>Hi, I'm Adelana. A software developer and a huge JavaScript fan.</p>
+            <p>Welcome to my portfolio website.</p>
+            <ContactMenu className="hero" />
+          </div>
+          <div className="hero__image" />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Hero;
