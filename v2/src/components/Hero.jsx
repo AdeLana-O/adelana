c363b20e8417dc520732e1a6c5d1af1e3537f9ee
ade_lana <lanadvictor@yahoo.co.uk>
2021-09-15 18:34:33 +0100

    Hero section changes

--- v2/src/components/Hero.jsx
+++ v2/src/components/Hero.jsx
@@ -1,49 +1,29 @@
 import React from "react";
 
-import { useStaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
-
 import ContactMenu from "./ContactMenu";
 
-const Hero = () => {
-  const data = useStaticQuery(query);
-
-  return (
-    <section id="hero">
-      <div className="container">
-        <div className="hero__container">
-          <div className="hero__info">
+const Hero = () => (
+  <section id="hero">
+    <div className="container">
+      <div className="hero__container">
+        <div className="hero__info">
+          <div>
             <h1>Hi, I'm Adelana Onafuwa.</h1>
-            <p>Software developer and big JavaScript fan.</p>
+          </div>
+          <div>
+            <p>Software developer of all things JavaScript.</p>
             <p>
-              Welcome to my personal website where you can find out details about me
-              and also projects I've worked on.
+              Welcome to my personal website. Here you will find out details about me
+              and projects I've worked on.
             </p>
           </div>
-          <div className="hero__links">
-            <ContactMenu />
-          </div>
         </div>
-        <Img
-          fluid={data.file.childImageSharp.fluid}
-          alt="Adelana Onafuwa"
-          className="hero__image"
-        />
+        <div className="hero__links">
+          <ContactMenu />
+        </div>
       </div>
-    </section>
-  );
-};
-
-const query = graphql`
-  query ProfileImage {
-    file(relativePath: { eq: "profile.jpg" }) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-  }
-`;
+    </div>
+  </section>
+);
 
 export default Hero;
