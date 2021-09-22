babf6007b1d13b9890da25878e61754a60c33e92
ade_lana <lanadvictor@yahoo.co.uk>
2021-09-22 17:17:17 +0100

    About section

--- v2/src/pages/index.js
+++ v2/src/pages/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-import SEO from "../components/SEO";
 import Layout from "../components/Layout";
+import SEO from "../components/SEO";
 import Hero from "../components/Hero";
+import About from "../components/About";
 import Work from "../components/Work";
 
 const Home = () => (
   <Layout>
     <SEO seo />
     <Hero />
+    <About />
     <Work />
   </Layout>
 );
