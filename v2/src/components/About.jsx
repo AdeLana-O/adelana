0a2639cfada0adbdaa53021de904ab32125a725e
ade_lana <lanadvictor@yahoo.co.uk>
2021-09-25 01:58:19 +0100

    removed unnecessary graphql query

--- v2/src/components/About.jsx
+++ v2/src/components/About.jsx
@@ -1,60 +1,43 @@
 import React from "react";
-import { graphql, useStaticQuery } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 
-const About = () => {
-  const data = useStaticQuery(query);
-
-  return (
-    <section id="about">
-      <div className="container">
-        <div className="about__container">
-          <div className="about__header">
-            <h2>About</h2>
-          </div>
-          <div className="about__details">
-            <p>
-              Fascinated by technology from a young age, I always knew I'd end up
-              with a career in tech.
-            </p>
-            <p>
-              I started my programming journey with PHP but I soon moved on after I
-              discovered the awesomeness that is <strong>JavaScript</strong>.
-            </p>
-            <p>
-              This is why I've made it a mission to achieve mastery of fullstack
-              JavaScript(TypeScript included) and its rich ecosystem of libraries and
-              frameworks.
-            </p>
-            <p>
-              As a result I have since built and contributed to projects using
-              libraries like React and Express.
-            </p>
-            <p>
-              I'm really eager to see what the future has in store for me as I look
-              to get involved in open source projects while at the same time,
-              stretching my legs into the world of Vue and Angular.
-            </p>
-          </div>
-          <div className="about__image">
-            <StaticImage src="../images/profile.jpg" alt="Adelana Onafuwa" />
-          </div>
+const About = () => (
+  <section id="about">
+    <div className="container">
+      <div className="about__container">
+        <div className="about__header">
+          <h2>About</h2>
+        </div>
+        <div className="about__details">
+          <p>
+            Fascinated by technology from a young age, I always knew I'd end up with
+            a career in tech.
+          </p>
+          <p>
+            I started my programming journey with PHP but I soon moved on after I
+            discovered the awesomeness that is <strong>JavaScript</strong>.
+          </p>
+          <p>
+            This is why I've made it a mission to achieve mastery of fullstack
+            JavaScript(TypeScript included) and its rich ecosystem of libraries and
+            frameworks.
+          </p>
+          <p>
+            As a result I have since built and contributed to projects using
+            libraries like React and Express.
+          </p>
+          <p>
+            I'm really eager to see what the future has in store for me as I look to
+            get involved in open source projects while at the same time, stretching
+            my legs into the world of Vue and Angular.
+          </p>
+        </div>
+        <div className="about__image">
+          <StaticImage src="../images/profile.jpg" alt="Adelana Onafuwa" />
         </div>
       </div>
-    </section>
-  );
-};
-
-const query = graphql`
-  query ProfileImage {
-    file(relativePath: { eq: "profile.jpg" }) {
-      childImageSharp {
-        fluid {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-  }
-`;
+    </div>
+  </section>
+);
 
 export default About;
