2e3a2c0111d7d0d2800e9e7efccb175647c3ae47
ade_lana <lanadvictor@yahoo.co.uk>
2021-10-26 21:27:12 +0100

    styled the Hero section

--- v2/src/components/Hero.jsx
+++ v2/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 import ContactMenu from "./ContactMenu";
 
@@ -6,20 +7,23 @@ const Hero = () => (
   <section id="hero">
     <div className="container">
       <div className="hero__container">
-        <div className="hero__info">
-          <div>
+        <div className="hero__details">
+          <div className="hero__info">
             <h1>Hi, I'm Adelana Onafuwa.</h1>
-          </div>
-          <div>
-            <p>JavaScript Software developer.</p>
+            <p>
+              <strong>JavaScript Software developer.</strong>
+            </p>
             <p>
               Welcome to my personal website. Here you will find out details about me
               and projects I've worked on.
             </p>
           </div>
+          <div className="hero__links">
+            <ContactMenu />
+          </div>
         </div>
-        <div className="hero__links">
-          <ContactMenu />
+        <div className="hero__image">
+          <StaticImage src="../images/profile.png" alt="Adelana Onafuwa" />
         </div>
       </div>
     </div>
