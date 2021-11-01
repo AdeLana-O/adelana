3830b7d0c4168673db361e76e33df1518a5eff70
ade_lana <lanadvictor@yahoo.co.uk>
2021-11-01 19:41:58 +0100

    implement new reusable functions and components

--- v2/src/components/WorkLarge.jsx
+++ v2/src/components/WorkLarge.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import withSlides from "../HOC/withSlides";
+import MobileDevice from "./MobileDevice";
+import SlideButtons from "./SlideButtons";
 import flattenImages from "../utils/flattenImages";
+import renderSlides from "../utils/renderSlides";
 
 const WorkLarge = ({
   data,
@@ -14,129 +16,29 @@ const WorkLarge = ({
 }) => {
   const imgTitle = data[currentIndex].name;
 
-  const renderMobile = data.map((img, index) => {
-    const { id, name, childImageSharp } = img.mobile.node;
-    const image = getImage(childImageSharp);
-    let className;
+  const dataRenderer = data.map(item => item.mobile);
 
-    const cbRef = element => {
-      if (imageElements.current && Array.isArray(imageElements.current)) {
-        if (element && !imageElements.current.includes(element)) {
-          imageElements.current.push(element);
-        }
-      }
-    };
+  const slides = data.map(item => item.desktop);
 
-    if (clicked && currentIndex < prevIndex.current && currentIndex === index) {
-      className = "slide__image slide__left";
-    } else if (
-      clicked &&
-      currentIndex > prevIndex.current &&
-      currentIndex === index
-    ) {
-      className = "slide__image slide__right";
-    } else if (
-      clicked &&
-      currentIndex < prevIndex.current &&
-      prevIndex.current === index
-    ) {
-      className = "slide__image slide__prev__left";
-    } else if (
-      clicked &&
-      currentIndex > prevIndex.current &&
-      prevIndex.current === index
-    ) {
-      className = "slide__image slide__prev__right";
-    } else if (currentIndex === index) {
-      className = "slide__image slide__active";
-    } else if (prevIndex.current === index) {
-      className = "slide__image slide__prev";
-    } else {
-      className = "slide__image";
-    }
-
-    return (
-      <div className={className} key={id} ref={cbRef}>
-        <GatsbyImage image={image} alt={name} />
-      </div>
-    );
-  });
-
-  const renderDesktop = data.map((img, index) => {
-    const { id, name, childImageSharp } = img.desktop.node;
-    const image = getImage(childImageSharp);
-    let className;
-
-    const cbRef = element => {
-      if (imageElements.current && Array.isArray(imageElements.current)) {
-        if (element && !imageElements.current.includes(element)) {
-          imageElements.current.push(element);
-        }
-      }
-    };
-
-    if (clicked && currentIndex < prevIndex.current && currentIndex === index) {
-      className = "slide__image slide__left";
-    } else if (
-      clicked &&
-      currentIndex > prevIndex.current &&
-      currentIndex === index
-    ) {
-      className = "slide__image slide__right";
-    } else if (
-      clicked &&
-      currentIndex < prevIndex.current &&
-      prevIndex.current === index
-    ) {
-      className = "slide__image slide__prev__left";
-    } else if (
-      clicked &&
-      currentIndex > prevIndex.current &&
-      prevIndex.current === index
-    ) {
-      className = "slide__image slide__prev__right";
-    } else if (currentIndex === index) {
-      className = "slide__image slide__active";
-    } else if (prevIndex.current === index) {
-      className = "slide__image slide__prev";
-    } else {
-      className = "slide__image";
-    }
-
-    return (
-      <div className={className} key={id} ref={cbRef}>
-        <GatsbyImage image={image} alt={name} />
-      </div>
-    );
-  });
-
-  const renderButtons = data.map(({ name }, index) => {
-    let className;
-
-    if (currentIndex === index) {
-      className = "slide__btn slide__btn--active";
-    } else {
-      className = "slide__btn";
-    }
-
-    return (
-      <li className="slide__btn__item" key={index}>
-        <button
-          aria-label={`Select ${name}`}
-          className={className}
-          onClick={() => changeImage(index)}
-        />
-      </li>
-    );
+  const renderDesktop = renderSlides({
+    data: slides,
+    currentIndex,
+    prevIndex,
+    clicked,
+    imageElements,
   });
 
   return (
     <div className="desktop__view__container">
       <div className="device__container">
         <div className="mobile__device__container">
-          <div className="mobile__device">
-            <div className="slide__images__container">{renderMobile}</div>
-          </div>
+          <MobileDevice
+            images={dataRenderer}
+            currentIndex={currentIndex}
+            prevIndex={prevIndex}
+            clicked={clicked}
+            imageElements={imageElements}
+          />
         </div>
         <div className="desktop__device__container">
           <div className="desktop__device">
@@ -148,7 +50,11 @@ const WorkLarge = ({
         <span>{imgTitle}</span>
       </div>
       <div className="slide__btns">
-        <ul className="slide__btns__container">{renderButtons}</ul>
+        <SlideButtons
+          images={dataRenderer}
+          currentIndex={currentIndex}
+          changeImage={changeImage}
+        />
       </div>
     </div>
   );
--- v2/src/components/WorkMobile.jsx
+++ v2/src/components/WorkMobile.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import withSlides from "../HOC/withSlides";
+import MobileDevice from "./MobileDevice";
+import SlideButtons from "./SlideButtons";
 import filterImages from "../utils/filterImages";
 
 const WorkMobile = ({
@@ -16,96 +17,30 @@ const WorkMobile = ({
     .replace(/-mobile/, "")
     .replace("-", " ");
 
-  const renderImages = data.map((img, index) => {
-    const { id, name, childImageSharp } = img.node;
-
-    const cbRef = element => {
-      if (imageElements.current && Array.isArray(imageElements.current)) {
-        if (element && !imageElements.current.includes(element)) {
-          imageElements.current.push(element);
-        }
-      }
-    };
-
-    const image = getImage(childImageSharp);
-
-    let className;
-
-    if (clicked && currentIndex < prevIndex.current && currentIndex === index) {
-      className = "slide__image slide__left";
-    } else if (
-      clicked &&
-      currentIndex > prevIndex.current &&
-      currentIndex === index
-    ) {
-      className = "slide__image slide__right";
-    } else if (
-      clicked &&
-      currentIndex < prevIndex.current &&
-      prevIndex.current === index
-    ) {
-      className = "slide__image slide__prev__left";
-    } else if (
-      clicked &&
-      currentIndex > prevIndex.current &&
-      prevIndex.current === index
-    ) {
-      className = "slide__image slide__prev__right";
-    } else if (currentIndex === index) {
-      className = "slide__image slide__active";
-    } else if (prevIndex.current === index) {
-      className = "slide__image slide__prev";
-    } else {
-      className = "slide__image";
-    }
-
-    return (
-      <div className={className} key={id} ref={cbRef}>
-        <GatsbyImage image={image} alt={name} />
-      </div>
-    );
-  });
-
-  const renderBtns = data.map((img, index) => {
-    const { id, name } = img.node;
-    const label = name.replace(/-mobile/, "").replace("-", " ");
-    let className;
-
-    if (currentIndex === index) {
-      className = "slide__btn slide__btn--active";
-    } else {
-      className = "slide__btn";
-    }
-
-    return (
-      <li className="slide__btn__item" key={id}>
-        <button
-          aria-label={`Select ${label}`}
-          className={className}
-          onClick={() => changeImage(index)}
-        />
-      </li>
-    );
-  });
-
   return (
     <div className="mobile__view__container">
       <div className="device__container">
-        <div className="mobile__device">
-          <div className="slide__images__container">{renderImages}</div>
-        </div>
+        <MobileDevice
+          images={data}
+          currentIndex={currentIndex}
+          prevIndex={prevIndex}
+          clicked={clicked}
+          imageElements={imageElements}
+        />
       </div>
       <div className="slide__image__title">
         <span>{imgTitle}</span>
       </div>
       <div className="slide__btns">
-        <ul className="slide__btns__container">{renderBtns}</ul>
+        <SlideButtons
+          images={data}
+          currentIndex={currentIndex}
+          changeImage={changeImage}
+        />
       </div>
     </div>
   );
 };
-
-export default withSlides(WorkMobile, filterImages);
 
 WorkMobile.propTypes = {
   data: PropTypes.array.isRequired,
@@ -115,3 +50,5 @@ WorkMobile.propTypes = {
   imageElements: PropTypes.object.isRequired,
   changeImage: PropTypes.func.isRequired,
 };
+
+export default withSlides(WorkMobile, filterImages);
