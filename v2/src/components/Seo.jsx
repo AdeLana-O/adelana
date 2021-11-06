cda3af2c49ed9faed006a290bba2e8652badabba
ade_lana <lanadvictor@yahoo.co.uk>
2021-11-06 18:36:41 +0100

    ...

--- v2/src/components/Seo.jsx
+++ v2/src/components/Seo.jsx
@@ -10,6 +10,7 @@ const query = graphql`
       siteMetadata {
         title
         description
+        siteUrl
         twitter
       }
     }
@@ -25,6 +26,7 @@ function Seo({ seo, pageDescription, pageTitle }) {
   const metaTitle = pageTitle ? `${pageTitle} | ${title}` : title;
   const metaDescription = pageDescription || description;
 
+  const isDemo = siteUrl.includes("demo");
   const imageUrl = `${siteUrl}/logo.svg`;
   const pageUrl = `${siteUrl}${pathname}`;
 
@@ -32,7 +34,12 @@ function Seo({ seo, pageDescription, pageTitle }) {
     <Helmet>
       <html lang="en" />
       <title>{metaTitle}</title>
-      <meta name="robots" content={seo ? "index, follow" : "noindex, follow"} />
+      <meta
+        name="robots"
+        content={
+          isDemo ? "noindex, nofollow" : seo ? "index, follow" : "noindex, follow"
+        }
+      />
       <meta name="author" content={title} />
       <meta name="description" content={metaDescription} />
       {seo && <meta name="og:title" content={metaTitle} />}
