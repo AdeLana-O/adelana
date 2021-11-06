9a5c5efb0922abded175a787d77ab7b5ed093886
ade_lana <lanadvictor@yahoo.co.uk>
2021-11-06 22:28:56 +0100

    Projects Tab

--- v2/src/components/ProjectsTab.jsx
+++ v2/src/components/ProjectsTab.jsx
@@ -1,22 +1,59 @@
 import React from "react";
 
 const ProjectsTab = ({ projectType, setProjectType }) => {
-  const changeProject = () => {
-    if (projectType === "project") {
-      setProjectType("side project");
-    } else {
-      setProjectType("project");
-    }
+  const setToAll = () => {
+    setProjectType("all");
+  };
+
+  const setToProjects = () => {
+    setProjectType("projects");
+  };
+
+  const setToSideProjects = () => {
+    setProjectType("side projects");
   };
 
   return (
-    <section id="projects-tab">
-      <div className="container">
-        <div className="projects-tab__container">
-          <p>Projects Tab</p>
-        </div>
-      </div>
-    </section>
+    <div className="projects-tab">
+      <ul className="projects-tab__btns">
+        <li className="projects-tab__item">
+          <button
+            onClick={setToAll}
+            className={
+              projectType === "all"
+                ? "projects-tab__btn projects-tab__btn--active"
+                : "projects-tab__btn"
+            }
+          >
+            All
+          </button>
+        </li>
+        <li className="projects-tab__item">
+          <button
+            onClick={setToProjects}
+            className={
+              projectType === "projects"
+                ? "projects-tab__btn projects-tab__btn--active"
+                : "projects-tab__btn"
+            }
+          >
+            Projects
+          </button>
+        </li>
+        <li className="projects-tab__item">
+          <button
+            onClick={setToSideProjects}
+            className={
+              projectType === "side projects"
+                ? "projects-tab__btn projects-tab__btn--active"
+                : "projects-tab__btn"
+            }
+          >
+            Side Projects
+          </button>
+        </li>
+      </ul>
+    </div>
   );
 };
 
