4fff45b7db3b611695e09756da29af81cd1506c9
ade_lana <lanadvictor@yahoo.co.uk>
2021-11-16 02:10:46 +0100

    ..

--- v2/gatsby-config.js
+++ v2/gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
         display: "standalone",
         icon: "static/logo.svg",
         icon_options: {
-          purpose: "maskable",
+          purpose: "any",
         },
       },
     },
