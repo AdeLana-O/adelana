45db18a9520bd82da639d3c344dee8c2b0b50eda
ade_lana <lanadvictor@yahoo.co.uk>
2021-11-29 00:19:26 +0100

    styling

--- v2/gatsby-browser.js
+++ v2/gatsby-browser.js
@@ -2,7 +2,6 @@ import React from "react";
 import Layout from "./src/components/Layout";
 import ThemeProvider from "./src/components/ThemeProvider";
 import "./src/styles/main.scss";
-import "./src/styles/darkTheme.scss";
 
 export const wrapPageElement = ({ element, props }) => (
   <Layout {...props}>{element}</Layout>
